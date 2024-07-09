316826b8e8190193085ef94c71a2d339e8ebab21
andrei G <sinezub@yandex.ru>
2024-07-10 00:20:44 +0300

    Fix tg login

--- src/components/TelegramLoginBtn.vue
+++ src/components/TelegramLoginBtn.vue
@@ -1,29 +1,15 @@
-<template>
-    <!-- Callback mode -->
-    <span v-if="!isLoaded">Loading...</span>
-    <telegram-login-temp mode="callback" telegram-login="genplan_estate_bot" @loaded='telegramLoadedCallbackFunc'
-        @callback="yourCallbackFunction" />
+<script lang="ts" setup>
+import { LoginWidget } from 'vue-tg'
+import type { LoginWidgetUser } from 'vue-tg'
 
-    <!-- Redirect mode -->
-    <telegram-login-temp mode="redirect" telegram-login="genplan_estate_bot" @loaded='telegramLoadedCallbackFunc'
-        redirect-url="http://genplan_dev.zae.life" />
-</template>
-
-<script setup lang="js">
-import { telegramLoginTemp } from 'vue3-telegram-login'
-import { ref } from 'vue'
-
-const isLoaded = ref(false)
-
-function telegramLoadedCallbackFunc() {
-    console.log('script is loaded')
-    isLoaded.value = true
-}
-
-function yourCallbackFunction(user) {
-    // gets user as an input
-    // id, first_name, last_name, username,
-    // photo_url, auth_date and hash
-    console.log(user)
+function handleUserAuth(user: LoginWidgetUser) {
+  console.log(user)
 }
 </script>
+
+<template>
+  <LoginWidget 
+    bot-username="genplan_estate_bot"
+    @auth="handleUserAuth"
+  />
+</template>
