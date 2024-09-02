e13f5b2a70601bfe7662c6555f152416f416ce9f
andrei G <sinezub@yandex.ru>
2024-09-02 09:29:43 +0300

    Fix sidebar

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -52,16 +52,8 @@ const Polygon = defineAsyncComponent(() =>
     <template #icon>
       <EcosystemIcon />
     </template>
-    <template #heading>Написать нам</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
+    <template #heading>Получай заявки</template>
+    <p>После добавления кода виждета ты сможешь сразу получить готовую карту с участками.</p>
   </WelcomeItem>
 </template>
 
